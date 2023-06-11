4fd71d603d9a113777387cdd6e8d9833aa16590a
meichtry <cedric.meichtry@gmail.com>
2023-06-11 10:59:42 -0400

    Final changes2

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -8,24 +8,33 @@ class TestEnglishToFrench(unittest.TestCase):
     """
     This class is for the english_to_french  tests
     """
-    def test1(self):
+    def test_equal(self):
         """
         This function tests the english_to_french function
         """
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
         self.assertEqual(english_to_french('Tomorrow'), 'Demain')
+
+    def test_not_equal(self):
+        """
+        This function tests the english_to_french function
+        """
         self.assertNotEqual(english_to_french('Hello'), 'Demain')
 
 class TestFrenchToEnglish(unittest.TestCase):
     """
     This class is for the french_to_english  tests
     """
-    def test1(self):
+    def test_equal(self):
         """
         This function tests the french_to_english function
         """
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
         self.assertEqual(french_to_english('demain'), 'tomorrow')
+
+    def test_not_equal(self):
+        """
+        This function tests the french_to_english function
+        """
         self.assertNotEqual(french_to_english('Demain'), 'Hello')
 
 unittest.main()
